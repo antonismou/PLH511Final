d9eedc85e13de769ca001901ed5d2facf18f15f8
antonismou <46896705+antonismou@users.noreply.github.com>
2025-11-30 17:37:11 +0200

    Change the .h file to has all i need

--- SimpleRoutingTree.h
+++ SimpleRoutingTree.h
@@ -12,21 +12,45 @@ enum{
 	TIMER_PERIOD_MILLI=150000,
 	TIMER_FAST_PERIOD=200,
 	TIMER_LEDS_MILLI=1000,
+
+	//ADDED
+	WINDOW_MILLI=80,
+	EPOCH_PERIOD_MILLI=20000,
+	AGGREGATION_TYPE_MIN=1,
+	AGGREGATION_TYPE_SUM=2,
+	AGGREGATION_TYPE_AVG=3,
+	//END ADDED
 };
 /*uint16_t AM_ROUTINGMSG=AM_SIMPLEROUTINGTREEMSG;
 uint16_t AM_NOTIFYPARENTMSG=AM_SIMPLEROUTINGTREEMSG;
 */
-typedef nx_struct RoutingMsg
-{
+//ADDED
+typedef nx_struct AggregationMin {
+	nx_uint16_t epoch;      // epoch number
+	nx_uint16_t minVal;     // minimum value (used for MIN) - changed from uint8_t to uint16_t
+	nx_uint16_t senderID;   // id of node sending this msg (optional, for debug)
+} AggregationMin;
+
+typedef nx_struct AggregationSUM {
+	nx_uint16_t epoch;      // epoch number
+	nx_uint16_t sum;      // sum value (used for SUM)
+	nx_uint16_t senderID;   // id of node sending this msg (optional, for debug)
+} AggregationSUM;
+
+typedef nx_struct AggregationAVG {
+	nx_uint16_t epoch;      // epoch number
+	nx_uint16_t sum;        // sum value (used for AVG)
+	nx_uint16_t count;      // count of values (used for AVG)
+	nx_uint16_t senderID;   // id of node sending this msg (optional, for debug)
+} AggregationAVG;
+//END ADDED
+
+
+typedef nx_struct RoutingMsg{
 	nx_uint16_t senderID;
 	nx_uint8_t depth;
+	nx_uint8_t aggType;     // ADDED
 } RoutingMsg;
 
-typedef nx_struct NotifyParentMsg
-{
-	nx_uint16_t senderID;
-	nx_uint16_t parentID;
-	nx_uint8_t depth;
-} NotifyParentMsg;
 
 #endif
